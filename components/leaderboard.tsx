cb68030be78a9556574efe3a7aabbf5f59434b14
SIDDHARTHxSRIVASTAVA <srivastavasiddharth2003@gmail.com>
2025-02-12 18:33:05 +0530

    tab

--- components/leaderboard.tsx
+++ components/leaderboard.tsx
@@ -1,5 +1,5 @@
 "use client";
-
+import TabNavigation from "./tabNavigation";
 import { useState, useEffect, useCallback } from "react";
 import { Card } from "@/components/ui/card";
 import { Tabs, TabsList, TabsTrigger } from "@/components/ui/tabs";
@@ -270,16 +270,9 @@ export default function Leaderboard() {
           </div>
         </div>
 
-        <div className="mb-12 flex space-x-2">
-          {["Daily", "Weekly", "Monthly"].map((period) => (
-            <Button
-              variant="ghost"
-              className="text-gray-300 hover:text-white !hover:bg-white/10 transition-all duration-300 rounded-lg px-4 py-2"
-            >
-              {period}
-            </Button>
-          ))}
-        </div>
+        <section>
+          <TabNavigation />
+        </section>
 
         {/* Top Cards */}
         <div className="max-w-6xl mx-auto mt-8 mb-8 perspective-1000">
